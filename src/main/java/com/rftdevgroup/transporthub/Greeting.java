a68f71d6afa8cbc60f6b33ed0eebf1191ecfc7ff
Mark Gyozo Budai <xmarkbudaix@hotmail.com>
2017-09-25 15:36:04 +0200

    Hello world style REST backend code

---
+++ src/main/java/com/rftdevgroup/transporthub/Greeting.java
@@ -0,0 +1,11 @@
+package com.rftdevgroup.transporthub;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Greeting {
+    private final long id;
+    private final String content;
+}
