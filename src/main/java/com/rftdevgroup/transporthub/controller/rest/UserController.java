1d71f361f06a08aa4a746743ad4a87c8ddfe4cb9
Mark Gyozo Budai <xmarkbudaix@hotmail.com>
2017-10-02 13:38:51 +0200

    Mapping and controller renaming

--- src/main/java/com/rftdevgroup/transporthub/controller/rest/UserController.java
+++ src/main/java/com/rftdevgroup/transporthub/controller/rest/UserController.java
@@ -1,19 +1,21 @@
-package com.rftdevgroup.transporthub.controller;
+package com.rftdevgroup.transporthub.controller.rest;
 
 import com.rftdevgroup.transporthub.model.User;
 import com.rftdevgroup.transporthub.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class ApiController {
+@RequestMapping(value = "/api")
+public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/test")
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
     public List<User> users() {
         return userRepository.findAll();
     }
