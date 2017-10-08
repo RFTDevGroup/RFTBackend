71a78ab41723d8b06d76ded8f49b609090f48a3e
Mark Gyozo Budai <xmarkbudaix@hotmail.com>
2017-10-08 10:13:12 +0200

    Implemented basic in memory auth

---
+++ src/main/java/com/rftdevgroup/transporthub/controller/rest/LoginController.java
@@ -0,0 +1,19 @@
+package com.rftdevgroup.transporthub.controller.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout(HttpSession session) {
+        session.invalidate();
+    }
+}
