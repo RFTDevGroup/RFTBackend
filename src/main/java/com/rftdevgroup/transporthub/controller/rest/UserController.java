842f9f5bc9b8f43b62f895f30ee1af9d85061036
Mark Gyozo Budai <xmarkbudaix@hotmail.com>
2017-11-27 09:56:05 +0100

    Added methods to fetch individual users

--- src/main/java/com/rftdevgroup/transporthub/controller/rest/UserController.java
+++ src/main/java/com/rftdevgroup/transporthub/controller/rest/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * REST Controller class for providing endpoints to {@link User} related operations.
@@ -32,7 +33,7 @@ public class UserController {
     private Validators validators;
 
     @Secured(ADMIN)
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    @RequestMapping(value = "/users", method = GET)
     public List<UserDTO> users() {
         return userService.listUsers();
     }
@@ -53,13 +54,13 @@ public class UserController {
     @RequestMapping(value = "/user/update", method = RequestMethod.PUT)
     public Response updateSelf(Principal principal, @RequestBody UserUpdateDTO updateDTO) {
         Optional<ValidationErrors> errors = validators.validate(updateDTO);
-        if(errors.isPresent()){
+        if (errors.isPresent()) {
             return new Response(ResponseStatus.VALIDATION_ERROR, errors.get());
         }
         Optional<UserDTO> activeUser = userService.findAndMapUser(principal.getName(), UserDTO.class);
         if (activeUser.isPresent()) {
             UserDTO updatedUser = userService.updateUser(activeUser.get().getId(), updateDTO);
-            if(updateDTO != null){
+            if (updateDTO != null) {
                 return new Response(ResponseStatus.OK, updatedUser);
             } else {
                 return new Response(ResponseStatus.INTERNAL_ERROR, "Update process failed!");
@@ -71,17 +72,36 @@ public class UserController {
 
     @Secured(ADMIN)
     @RequestMapping(value = "/user/{id}/update", method = RequestMethod.PUT)
-    public Response updateUser(@PathVariable("id") long id, @RequestBody UserUpdateDTO updateDTO){
+    public Response updateUser(@PathVariable("id") long id, @RequestBody UserUpdateDTO updateDTO) {
         Optional<ValidationErrors> errors = validators.validate(updateDTO);
-        if(errors.isPresent()){
+        if (errors.isPresent()) {
             return new Response(ResponseStatus.VALIDATION_ERROR, errors.get());
         }
         UserDTO updatedUser = userService.updateUser(id, updateDTO);
-        if(updatedUser == null){
+        if (updatedUser == null) {
             return new Response(ResponseStatus.INTERNAL_ERROR, "User not found with given id.");
         } else {
             return new Response(ResponseStatus.OK, updatedUser);
         }
+    }
+
+    @Secured(USER)
+    @RequestMapping(value = "/user", method = GET)
+    public Response getSelf(Principal principal) {
+        String username = principal.getName();
+        Optional<UserDTO> userDTO = userService.findAndMapUser(username, UserDTO.class);
+        if (!userDTO.isPresent()) return new Response(ResponseStatus.INTERNAL_ERROR, "No user found!");
+
+        return new Response(ResponseStatus.OK, userDTO.get());
+    }
+
+    @Secured(ADMIN)
+    @RequestMapping(value = "/user/{id}", method = GET)
+    public Response adminGetUser(@PathVariable("id") long id) {
+        Optional<UserDTO> userDTO = userService.findAndMapUser(id, UserDTO.class);
+        if (!userDTO.isPresent()) return new Response(ResponseStatus.INTERNAL_ERROR, "No user found with given id!");
+
+        return new Response(ResponseStatus.OK, userDTO.get());
     }
 
 }
